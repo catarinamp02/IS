ebca4178e241e359fe973c9b6747543d4fdd0e95
Daniela Alves <alves@DESKTOP-9E7BD7O>
2025-05-17 15:49:51 +0100

    API proj

---
+++ API_SOAP/Financeiros.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="C#" CodeBehind="Financeiros.asmx.cs" Class="API_SOAP.Financeiros" %>
---
+++ API_SOAP/Financeiros.asmx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace API_SOAP
+{
+    /// <summary>
+    /// Summary description for Financeiros
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class Financeiros : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "API SOAP Financeira ativa!";
+        }
+    }
+}
