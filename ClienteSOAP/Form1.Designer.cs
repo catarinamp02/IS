4e59080dc9d76eee96ca1422bdde72512619355f
Daniela Alves <alves@DESKTOP-9E7BD7O>
2025-05-27 19:00:43 +0100

    cliente + ligação a serviço

---
+++ ClienteSOAP/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace ClienteSOAP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cbMetodo = new System.Windows.Forms.ComboBox();
+            this.pnlParametros = new System.Windows.Forms.Panel();
+            this.btnExecutar = new System.Windows.Forms.Button();
+            this.textResultado = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // cbMetodo
+            // 
+            this.cbMetodo.FormattingEnabled = true;
+            this.cbMetodo.Location = new System.Drawing.Point(47, 34);
+            this.cbMetodo.Name = "cbMetodo";
+            this.cbMetodo.Size = new System.Drawing.Size(121, 21);
+            this.cbMetodo.TabIndex = 0;
+            // 
+            // pnlParametros
+            // 
+            this.pnlParametros.Location = new System.Drawing.Point(47, 79);
+            this.pnlParametros.Name = "pnlParametros";
+            this.pnlParametros.Size = new System.Drawing.Size(282, 258);
+            this.pnlParametros.TabIndex = 1;
+            // 
+            // btnExecutar
+            // 
+            this.btnExecutar.Location = new System.Drawing.Point(200, 34);
+            this.btnExecutar.Name = "btnExecutar";
+            this.btnExecutar.Size = new System.Drawing.Size(75, 23);
+            this.btnExecutar.TabIndex = 2;
+            this.btnExecutar.Text = "Selecionar";
+            this.btnExecutar.UseVisualStyleBackColor = true;
+            // 
+            // textResultado
+            // 
+            this.textResultado.Location = new System.Drawing.Point(473, 79);
+            this.textResultado.Name = "textResultado";
+            this.textResultado.ReadOnly = true;
+            this.textResultado.Size = new System.Drawing.Size(280, 258);
+            this.textResultado.TabIndex = 3;
+            this.textResultado.Text = "";
+            this.textResultado.WordWrap = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textResultado);
+            this.Controls.Add(this.btnExecutar);
+            this.Controls.Add(this.pnlParametros);
+            this.Controls.Add(this.cbMetodo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cbMetodo;
+        private System.Windows.Forms.Panel pnlParametros;
+        private System.Windows.Forms.Button btnExecutar;
+        private System.Windows.Forms.RichTextBox textResultado;
+    }
+}
+
