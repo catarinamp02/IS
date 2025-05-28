9a05d7abb884d94b78c384793f9cb6e8efe965a3
catarinamp02 <catarinamp02@gmail.com>
2025-05-28 10:48:31 +0100

    stream configurada em ProductionLine

--- ProductionLine/Program.cs
+++ ProductionLine/Program.cs
@@ -1,4 +1,6 @@
 ﻿using RabbitMQ.Client;
+using RabbitMQ.Stream.Client;
+using RabbitMQ.Stream.Client.Reliable;
 using ProductionLine;
 using System.Text;
 using System.Text.Json;
@@ -7,7 +9,21 @@ var factory = new ConnectionFactory { HostName = "localhost" };
 using var connection = await factory.CreateConnectionAsync();
 using var channel = await connection.CreateChannelAsync();
 
+//queue
 await channel.ExchangeDeclareAsync(exchange: "productionLine", type: ExchangeType.Topic);
+
+
+//criara straem
+var streamSystem = await StreamSystem.Create(new StreamSystemConfig());
+
+await streamSystem.CreateStream(new StreamSpec("production-stream")
+{
+    MaxLengthBytes = 5_000_000_000
+});
+
+var producer = await Producer.Create(new ProducerConfig(streamSystem, "production-stream"));
+
+
 
 string routingKey;
 
@@ -36,10 +52,15 @@ while (true)
         routingKey = "dados.producao.desconhecido";
     }
 
-
+    //enviar para as filas
     await channel.BasicPublishAsync(exchange: "productionLine", routingKey: routingKey, body: body);
 
     Console.WriteLine($"Sent '{routingKey}':'{message}'");
+
+    //enviar para a stream
+    await producer.Send(new Message(body));
+
+    Console.WriteLine($"Sent to stream :'{message}'");
 
     //Tempo de produção em ms
     int tempo = peca.tempoProd * 1000;
